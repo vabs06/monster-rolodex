1714909d9a2fb3c1fe6794386b7f3b2b6ffcec6e
Vaibhav Malviya <vabs.malviya@gmail.com>
2020-07-27 16:06:17 +0530

    Updates

---
+++ precache-manifest.e126e80ee3a7f3156edb17eee00f0907.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "239048ee51899f7e9816f605bb88701f",
+    "url": "/monster-rolodex/index.html"
+  },
+  {
+    "revision": "9062f5b4b4390d864f12",
+    "url": "/monster-rolodex/static/css/main.b1a284bb.chunk.css"
+  },
+  {
+    "revision": "1bb5dd2a6657ff2ee685",
+    "url": "/monster-rolodex/static/js/2.b6f2604e.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/monster-rolodex/static/js/2.b6f2604e.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "9062f5b4b4390d864f12",
+    "url": "/monster-rolodex/static/js/main.99e7c181.chunk.js"
+  },
+  {
+    "revision": "90506cc279e62bb14608",
+    "url": "/monster-rolodex/static/js/runtime-main.59621b72.js"
+  }
+]);
